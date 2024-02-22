ea1265b4476ed2c0c51aa67a306a266633fb48ab
evan0416 <cycloevan97@gmail.com>
2024-02-22 22:15:34 +0900

    docs: Update app.py

--- Q&A-Chatbot/app.py
+++ Q&A-Chatbot/app.py
@@ -1,36 +1,29 @@
+# Q&A Chatbot
+from langchain.llms import OpenAI
 
-## Conversational Q&A Chatbot
+#from dotenv import load_dotenv
+
+#load_dotenv()  # take environment variables from .env.
+
 import streamlit as st
-
-from langchain.schema import HumanMessage,SystemMessage,AIMessage
-from langchain.chat_models import ChatOpenAI
-
-## Streamlit UI
-st.set_page_config(page_title="Conversational Q&A Chatbot")
-st.header("Hey, Let's Chat")
-
-from dotenv import load_dotenv
-load_dotenv()
 import os
 
-chat=ChatOpenAI(temperature=0.5)
-
-if 'flowmessages' not in st.session_state:
-    st.session_state['flowmessages']=[
-        SystemMessage(content="Yor are a comedian AI assitant")
-    ]
 
 ## Function to load OpenAI model and get respones
 
-def get_chatmodel_response(question):
+def get_openai_response(question):
+    llm=OpenAI(model_name="text-davinci-003",temperature=0.5)
+    response=llm(question)
+    return response
 
-    st.session_state['flowmessages'].append(HumanMessage(content=question))
-    answer=chat(st.session_state['flowmessages'])
-    st.session_state['flowmessages'].append(AIMessage(content=answer.content))
-    return answer.content
+##initialize our streamlit app
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Langchain Application")
 
 input=st.text_input("Input: ",key="input")
-response=get_chatmodel_response(input)
+response=get_openai_response(input)
 
 submit=st.button("Ask the question")
 
@@ -39,3 +32,4 @@ submit=st.button("Ask the question")
 if submit:
     st.subheader("The Response is")
     st.write(response)
+
